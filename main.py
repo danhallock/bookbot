1909878b41b4211ae502702c6ccbb90ad6e43a5e
Dan Hallock <dan@hallockfamily.name>
2025-08-12 00:03:45 -0700

    CH3 L1 Print a report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import count_words
 from stats import count_letters
+from stats import sort_letter_count
 
 def get_book_text(path):
 	with open(path) as b:
@@ -7,8 +8,19 @@ def get_book_text(path):
 		return text
 
 def main():
-	f = get_book_text('books/frankenstein.txt')
+	path = 'books/frankenstein.txt'
+	f = get_book_text(path)
+	w = count_words(f)
 	c = count_letters(f)
-	print(f"{c}")
+	s = sort_letter_count(c)
+	print("============ BOOKBOT ============")
+	print(f"Analyzing book found at {path}...")
+	print("----------- Word Count ----------")
+	print(f"Found {w} total words.")
+	print("--------- Character Count -------")
+	for letter in s:
+		if letter['letter'].isalpha():
+			print(f"{letter['letter']}: {letter['count']}")
+	print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,13 @@ def count_letters(text):
 		else:
 			per_letter_count[c] = 1
 	return per_letter_count
+
+def sort_on_letter_count(letter_count):
+	return letter_count["count"]
+
+def sort_letter_count(letter_count):
+	sortable_list = []
+	for letter, count in letter_count.items():
+		sortable_list.append({'letter': letter, 'count': count})
+	sortable_list.sort(reverse=True, key=sort_on_letter_count)
+	return sortable_list
